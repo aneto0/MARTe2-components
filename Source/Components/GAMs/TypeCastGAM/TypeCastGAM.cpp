f30f1760aa04f837327a34d34982bebb5c3eea84
André Neto <Andre.Neto@f4e.europa.eu>
2017-01-16 19:26:42 +0100

    Draft version of TypeCastGAM.

---
+++ Source/Components/GAMs/TypeCastGAM/TypeCastGAM.cpp
@@ -0,0 +1,165 @@
+/**
+ * @file TypeCastGAM.cpp
+ * @brief Source file for class TypeCastGAM
+ * @date 16/01/2017
+ * @author Andre Neto
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class TypeCastGAM (public, protected, and private). Be aware that some
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+#include "AdvancedErrorManagement.h"
+
+#include "TypeCastGAM.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+namespace MARTe {
+TypeCastGAM::TypeCastGAM() :
+        GAM() {
+    signalNumberOfElements = NULL;
+    signalNumberOfSamples = NULL;
+    inputSignalType = NULL;
+    outputSignalType = NULL;
+    numberOfSignals = 0u;
+}
+
+TypeCastGAM::~TypeCastGAM() {
+    if (signalNumberOfElements != NULL) {
+        delete[] signalNumberOfElements;
+    }
+    if (signalNumberOfSamples != NULL) {
+        delete[] signalNumberOfSamples;
+    }
+    if (inputSignalType != NULL) {
+        delete[] inputSignalType;
+    }
+    if (outputSignalType != NULL) {
+        delete[] outputSignalType;
+    }
+}
+
+bool TypeCastGAM::Setup() {
+    bool ret = (GetNumberOfInputSignals() == GetNumberOfOutputSignals());
+    if (!ret) {
+        REPORT_ERROR(ErrorManagement::InitialisationError, "GetNumberOfInputSignals() != GetNumberOfOutputSignals()");
+    }
+    uint32 n;
+    if (ret) {
+        numberOfSignals = GetNumberOfInputSignals();
+        signalNumberOfElements = new uint32[numberOfSignals];
+        signalNumberOfSamples = new uint32[numberOfSignals];
+        inputSignalType = new TypeDescriptor[numberOfSignals];
+        outputSignalType = new TypeDescriptor[numberOfSignals];
+    }
+
+    for (n = 0u; (n < GetNumberOfInputSignals()) && (ret); n++) {
+        uint32 inSamples = 1u;
+        uint32 outSamples = 1u;
+        uint32 idx = n;
+        ret = GetSignalNumberOfSamples(InputSignals, idx, inSamples);
+        if (ret) {
+            ret = GetSignalNumberOfSamples(OutputSignals, idx, outSamples);
+        }
+        if (ret) {
+            ret = (inSamples == outSamples);
+            if (!ret) {
+                REPORT_ERROR_PARAMETERS(ErrorManagement::InitialisationError,
+                                        "GetSignalNumberOfSamples(InputSignals, %d) != GetSignalNumberOfSamples(OutputSignals, %d)", idx, idx)
+            }
+        }
+        if (ret) {
+            signalNumberOfSamples[idx] = inSamples;
+        }
+
+        uint32 inElements = 1u;
+        uint32 outElements = 1u;
+
+        if (ret) {
+            ret = GetSignalNumberOfElements(InputSignals, idx, inElements);
+        }
+        if (ret) {
+            ret = GetSignalNumberOfElements(OutputSignals, idx, outElements);
+        }
+        if (ret) {
+            ret = (inElements == outElements);
+            if (!ret) {
+                REPORT_ERROR_PARAMETERS(ErrorManagement::InitialisationError,
+                                        "GetSignalNumberOfElements(InputSignals, %d) != GetSignalNumberOfElements(OutputSignals, %d)", idx, idx)
+            }
+        }
+        if (ret) {
+            signalNumberOfElements[idx] = inElements;
+        }
+
+        TypeDescriptor inType = GetSignalType(InputSignals, idx);
+        TypeDescriptor outType = GetSignalType(OutputSignals, idx);
+        if (ret) {
+            inputSignalType[idx] = inType;
+            outputSignalType[idx] = outType;
+        }
+    }
+
+    return ret;
+}
+
+bool TypeCastGAM::Execute() {
+    uint32 i;
+    uint32 s;
+    uint32 n;
+    //TODO finish all cases
+    for (i = 0u; i < numberOfSignals; i++) {
+        void *inputSignal = GetInputSignalMemory(i);
+        void *outputSignal = GetOutputSignalMemory(i);
+        if (inputSignalType[i] == UnsignedInteger16Bit) {
+            if (outputSignalType[i] == UnsignedInteger32Bit) {
+                for (s = 0u; s < signalNumberOfSamples[i]; s++) {
+                    for (n = 0u; n < signalNumberOfElements[i]; n++) {
+                        uint32 *dest = reinterpret_cast<uint32 *>(outputSignal);
+                        uint16 *src = reinterpret_cast<uint16 *>(inputSignal);
+                        dest[s * signalNumberOfElements[i] + n] = src[s * signalNumberOfElements[i] + n];
+                    }
+                }
+            }
+            else if (outputSignalType[i] == Float32Bit) {
+                for (s = 0u; s < signalNumberOfSamples[i]; s++) {
+                    for (n = 0u; n < signalNumberOfElements[i]; n++) {
+                        float32 *dest = reinterpret_cast<float32 *>(outputSignal);
+                        uint16 *src = reinterpret_cast<uint16 *>(inputSignal);
+                        dest[s * signalNumberOfElements[i] + n] = src[s * signalNumberOfElements[i] + n];
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
+CLASS_REGISTER(TypeCastGAM, "1.0")
+}
+
---
+++ Source/Components/GAMs/TypeCastGAM/TypeCastGAM.h
@@ -0,0 +1,134 @@
+/**
+ * @file TypeCastGAM.h
+ * @brief Header file for class TypeCastGAM
+ * @date 16/01/2017
+ * @author Andre Neto
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class TypeCastGAM
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef TYPE_CAST_GAM_H_
+#define TYPE_CAST_GAM_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+#include "GAM.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief TODO TODO
+ *
+ * GAM which copies its inputs to its outputs. Allows to plug different DataSources (e.g. driver with a DDB).
+ * @details This GAM copies its inputs to its outputs. The number of inputs shall
+ *  be exactly the number of the outputs and, for each signal, the types shall also be the same.
+ *  Given that the DataSources cannot interchange data directly between them the main scope of the TypeCastGAM is to
+ *  serve as a (direct) connector between DataSources.
+ *
+ * The configuration syntax is (names and signal quantity are only given as an example):
+ * +Buffer = {
+ *     Class = TypeCastGAM
+ *     InputSignals = {
+ *         Signal1 = {
+ *             DataSource = "Drv1"
+ *             Type = uint32
+ *         }
+ *         Signal2 = {
+ *             DataSource = "Drv2"
+ *             Type = int32
+ *         }
+ *     }
+ *     OutputSignals = {
+ *         Signal1O = {
+ *             DataSource = "LCD"
+ *             Type = uint32
+ *         }
+ *         Signal2O = {
+ *             DataSource = "LCD"
+ *             Type = int32
+ *         }
+ *     }
+ * }
+ */
+namespace MARTe {
+class TypeCastGAM: public GAM {
+public:
+    CLASS_REGISTER_DECLARATION()
+
+    /**
+     * @brief Constructor. NOOP.
+     */
+    TypeCastGAM();
+
+    /**
+     * @brief Destructor. NOOP.
+     */
+    virtual ~TypeCastGAM();
+
+    /**
+     * @brief TODO TODO
+     *
+     *
+     * Verifies correctness of the GAM configuration.
+     * @details Checks that the number of input signals is equal to the number of output signals and that,
+     * for each signal, the same type is used.
+     * @return is the pre-conditions are met.
+     * @pre
+     *   SetConfiguredDatabase() &&
+     *   GetNumberOfInputSignals() == GetNumberOfOutputSignals() &&
+     *   for each signal i: GetSignalType(InputSignals, i) == GetSignalType(OutputSignals, i) &&
+     *   for each signal i: GetSignalByteSize(InputSignals, i) * GetSignalNumberOfSamples(InputSignals, i) == GetSignalByteSize(OutputSignals, i) * GetSignalNumberOfSamples(OutputSignals, i)
+     */
+    virtual bool Setup();
+
+    /**
+     * @brief TODO TODO
+     * @brief Copies the input signals memory to the output signal memory.
+     * @return true if all the signals memory can be successfully copied.
+     */
+    virtual bool Execute();
+
+private:
+    //TODO
+    uint32 *signalNumberOfElements;
+
+    //TODO
+    uint32 *signalNumberOfSamples;
+
+    //TODO
+    TypeDescriptor *inputSignalType;
+
+    //TODO
+    TypeDescriptor *outputSignalType;
+
+    //TODO
+    uint32 numberOfSignals;
+};
+}
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+#endif /* TypeCastGAM_H_ */
+
