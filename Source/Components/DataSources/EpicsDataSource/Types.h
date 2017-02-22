3d532ee7bc0beda4583741b39e2927ed432d7cfc
Ivan Herrero <ivan.herrero@gtd.eu>
2017-02-22 19:41:31 +0100

    Fix flexelint code nº451 (Header file 'FileName' repeatedly included but does not have a standard include guard) and nº1708 (typedef 'Symbol' not declared as "C" conflicts with Location).

--- Source/Components/DataSources/EpicsDataSource/Types.h
+++ Source/Components/DataSources/EpicsDataSource/Types.h
@@ -29,7 +29,9 @@
 /*---------------------------------------------------------------------------*/
 
 /*lint -efile(766, Types.h) std::size_t is actually imported from cstddef*/
+/*lint -e451 */
 #include <cstddef>    //Import std::size_t
+/*lint +e451 */
 
 /*---------------------------------------------------------------------------*/
 /*                        Project header includes                            */
@@ -49,6 +51,8 @@
 #endif
 
 namespace SDA {
+
+/*lint -e1708*/
 
 /**
  * Type alias for unsigned integers
@@ -83,6 +87,8 @@ typedef std::size_t size_type;
  * Maximum value for unsigned integers of 32 bits
  */
 static const uint32 MAX_UINT32 = 0xFFFFFFFFU;
+
+/*lint +e1708*/
 
 }
 
