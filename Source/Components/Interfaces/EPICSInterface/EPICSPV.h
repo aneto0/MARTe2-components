ad43af61edb193b4cb473d540ea0b60db03aa595
André Neto <Andre.Neto@f4e.europa.eu>
2017-03-24 17:07:45 +0100

    Documentation in progress.

--- Source/Components/Interfaces/EPICSInterface/EPICSPV.h
+++ Source/Components/Interfaces/EPICSInterface/EPICSPV.h
@@ -40,7 +40,30 @@
 /*                           Class declaration                               */
 /*---------------------------------------------------------------------------*/
 namespace MARTe {
-
+/**
+ * @brief Describes an EPICS PV.
+ * @details This class wraps an EPICS PV, allowing to caput and caget values from it.
+ * This class is capable of triggering Messages as a response to a PV value change. The new PV value can
+ * either be: 1) the name of the Function to be called; 2) or an ID of a Function to be called (see FunctionMap);
+ *  3) or the input parameter of a pre-defined Function to be called; or 4) the value is to be ignored and a pre-defined Function is to be called with no parameters.
+ * The configuration syntax is (names are only given as an example):
+ * +PV_1 = {
+ *   Class = EPICSInterface::EPICSPV
+ *   PVName = PV_ONE //Compulsory. Name of the EPICS PV.
+ *   PVType = uint32 //Compulsory. The PV type.
+ *   Event = { //Optional. Information about the message to be triggered every-time the EPICS PV value.
+ *     Destination = StateMachine //Compulsory. Destination of the message.
+ *     PVValue = Function //Compulsory. Can either be Function, Parameter or Ignore.
+ *                        //If Function the PV value will be used as the name of the Function to call.
+ *                        //If Parameter the PV value will be used as the parameter of the Function to call. This implies that the Function parameter must be set.
+ *                        //If Ignore, the PV value will not be used and the Function will always be called.
+ *     Function = STOP //Compulsory if FunctionMap is not defined. Shall not be set if FunctionMap is defined.
+ *     FunctionMap = {{"1", "RUN"}, {"0", "STOP"}} //Optional if PVValue == Function. If defined then the PV value (first column of the matrix) will be used to map the Function name (second column of the matrix).
+ *   }
+ * }
+ *
+ * The CAPut and CAGet class methods are registered as call-backs. The parameter to put/get shall be encoded as param1 in a StructuredDataI attached to the message.
+ */
 class EPICSPV: public Object, public MessageI {
 public:
     CLASS_REGISTER_DECLARATION()
