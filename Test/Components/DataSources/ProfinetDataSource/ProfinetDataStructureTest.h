5571f51475c9ab25637faaa28f1ba2ec3374088d
marte <marte@centos7.marte>
2021-01-18 19:30:28 +0000

    Added GTests for ProfinetDataStructure. Minor modifications to ProfinetDataStructure.

--- Test/Components/DataSources/ProfinetDataSource/ProfinetDataStructureTest.h
+++ Test/Components/DataSources/ProfinetDataSource/ProfinetDataStructureTest.h
@@ -1,8 +1,8 @@
 /**
  * @file ProfinetDataStructureTest.h
  * @brief Header file for class ProfinetDataStructureTest
- * @date 
- * @author
+ * @date 14/01/2021
+ * @author Pedro Lourenco
  *
  * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
  * the Development of Fusion Energy ('Fusion for Energy').
@@ -11,7 +11,7 @@
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
  *
- * @warning Unless required by applicable law or agreed to in writing, 
+ * @warning Unless required by applicable law or agreed to in writing,
  * software distributed under the Licence is distributed on an "AS IS"
  * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the Licence permissions and limitations under the Licence.
@@ -32,24 +32,79 @@
 /*                        Project header includes                            */
 /*---------------------------------------------------------------------------*/
 
-
 /*---------------------------------------------------------------------------*/
 /*                           Class declaration                               */
 /*---------------------------------------------------------------------------*/
+
 /**
- * @brief Tests the MDSWriter public methods.
+ * @brief Tests the ProfinetDataStructure public methods.
  */
 class ProfinetDataStructureTest {
-    public:
-        bool TestConstructor();
-        
-    private:
+public:
 
+    /**
+     * @brief Tests the Constructor method.
+     */
+    bool TestConstructor();
+
+    /**
+     * @brief Tests the InsertHead method.
+     */
+    bool TestInsertHead();
+
+    /**
+     * @brief Tests the InsertTail method.
+     */
+    bool TestInsertTail();
+
+    /**
+     * @brief Tests the RemoveHead method.
+     */
+    bool TestRemoveHead();
+
+    /**
+     * @brief Tests the RemoveTail method.
+     */
+    bool TestRemoveTail();
+
+    /**
+     * @brief Tests the EmptyLinkedList method.
+     */
+    bool TestEmptyLinkedList();
+
+    /**
+     * @brief Tests the LinkedListStatus method.
+     */
+    bool TestLinkedListStatus();
+
+    /**
+     * @brief Tests the GetIterator method.
+     */
+    bool TestGetIterator();
+
+    /**
+     * @brief Tests the First method.
+     */
+    bool TestFirst();
+
+    /**
+     * @brief Tests the Next method.
+     */
+    bool TestNext();
+
+    /**
+     * @brief Tests the Last method.
+     */
+    bool TestLast();
+
+    /**
+     * @brief Tests the Previous method.
+     */
+    bool TestPrevious();
 };
 
 /*---------------------------------------------------------------------------*/
 /*                        Inline method definitions                          */
 /*---------------------------------------------------------------------------*/
 
-#endif /* MDSWRITER_MDSWRITERTEST_H_ */
-
+#endif /* DATASOURCES_PROFINET_PROFINETDATASTRUCTURETEST_H_ */
