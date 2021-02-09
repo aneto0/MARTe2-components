54045465f992592650eaf3c0074fd042c522fb04
Giuseppe Avon <giuseppe.avon@ext.f4e.europa.eu>
2021-02-09 19:00:33 +0100

    Constants added

---
+++ Source/Components/DataSources/ProfinetDataSource/ProfinetDataSourceConstants.h
@@ -0,0 +1,35 @@
+/**
+ * @file ProfinetDataSourceConstants.h
+ * @brief Header file for class ProfinetDataSourceConstants
+ * @date 15/01/2021
+ * @author Giuseppe Avon
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class ProfinetDataSourceConstants
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef __PROFINET_DATASOURCE_CONSTANTS_H__
+#define __PROFINET_DATASOURCE_CONSTANTS_H__
+
+    #ifndef PNET_MAX_PORT
+        #define PNET_MAX_PORT 1
+    #endif
+
+    #ifndef PNET_MAX_DIRECTORYPATH_SIZE
+        #define PNET_MAX_DIRECTORYPATH_SIZE 240
+    #endif
+
+#endif /* __PROFINET_DATASOURCE_CONSTANTS_H__ */
