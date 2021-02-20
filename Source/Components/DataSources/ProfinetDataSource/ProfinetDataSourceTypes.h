09da415a34dd87c358c6c83c6278144265c69bea
Giuseppe Avon <giuseppe.avon@ext.f4e.europa.eu>
2021-02-20 08:11:28 +0100

    Updated doxygen comments as per QA suggestions

--- Source/Components/DataSources/ProfinetDataSource/ProfinetDataSourceTypes.h
+++ Source/Components/DataSources/ProfinetDataSource/ProfinetDataSourceTypes.h
@@ -73,21 +73,53 @@ namespace ProfinetDataSourceDriver {
      *              When running, the two must match, otherwise no memory mapping can occur and no runtime plugging can happen.
     */
     typedef struct pnet_subslot {
+	/**
+ 	* @brief Indicates if the subslot belongs to the special DAP slots (see DataSource documentation)
+ 	*/ 
         bool                isDeviceAccessPoint;
+	/**
+ 	* @brief Indicates if the subslot was added by the master, by advertising it
+ 	*/ 	
         bool                masterAdded;
+	/**
+ 	* @brief Indicates if the subslot was added by the user, by configuring it in the MARTe2 configuration file
+ 	*/ 
         bool                configAdded;
+	/**
+ 	* @brief Indicates if the alarm for the subslot is armed (currently not supported)
+ 	*/ 	
         bool                alarmArmed;
-        
+        /*
+ 	* @brief Identifier number for the subslot
+ 	*/  
         uint16_t            subslotNumber;
+	/**
+ 	* @brief Identifier number for the submodule
+ 	*/ 	
         uint32_t            submoduleIdentifier;
+	/**
+ 	* @brief Identifier name for the submodule (unused)
+ 	*/
         std::string         submoduleName;
-        
+        /**
+ 	* @brief Memory bank for input data. This heap block is shared with the underlying p-net lib.
+ 	*/	
         uint8_t             *inputData;
+	/**
+ 	* @brief Memory bank for output data. This heap block is shared with the underlying p-net lib.
+ 	*/ 
         uint8_t             *outputData;
-
+	/**
+ 	* @brief Master advertised configuration data (must match with the expected to enable the slot for the plugging.
+ 	*/ 
         pnet_data_cfg_t     masterConfigurationData;
+	/**
+ 	* @brief MARTe2 configuration acquired data (must match with master to enable the slot for the plugging.
+ 	*/
         pnet_data_cfg_t     expectedConfigurationData;
-
+	/**
+ 	* @brief Constructs and initializes the subslot
+ 	*/ 
         pnet_subslot() {
             isDeviceAccessPoint = masterAdded = configAdded = alarmArmed = false;
             submoduleIdentifier = 0u;
@@ -103,20 +135,43 @@ namespace ProfinetDataSourceDriver {
             expectedConfigurationData.insize = 0u;
             expectedConfigurationData.outsize = 0u;
         }
-
+	/**
+ 	* @brief Checks if expected and master advertised configuration is matching
+ 	*/ 
         bool IsMatchingExpectedConfiguration() const { 
             return  (masterConfigurationData.data_dir ==  expectedConfigurationData.data_dir) &&
                     (masterConfigurationData.insize == expectedConfigurationData.insize) &&
                     (masterConfigurationData.outsize == expectedConfigurationData.outsize);
         }
-
+	/**
+ 	* @brief Convenient acceleration method to tell if the subslot is a DAP
+ 	*/ 
         bool  IsDAP()     const { return isDeviceAccessPoint; }
-        bool  IsInput()   const { return (masterConfigurationData.data_dir == PNET_DIR_INPUT) || (masterConfigurationData.data_dir == PNET_DIR_IO); }
-        bool  IsOutput()  const { return (masterConfigurationData.data_dir == PNET_DIR_OUTPUT) || (masterConfigurationData.data_dir == PNET_DIR_IO); }
-        bool  IsNoIO()    const { return (masterConfigurationData.data_dir == PNET_DIR_NO_IO); }
-        bool  IsUsed()    const { return (masterAdded && configAdded);  }
-        bool  IsAlarmed() const { return alarmArmed; }
-        //lint -e{1960} Alarm acknowledgement sets the armed variable back, which status can be retrieved with the IsAlarmed function
+        /**
+	* @brief Convenient acceleration method to tell if the subslot is an input
+	*/	 
+	bool  IsInput()   const { return (masterConfigurationData.data_dir == PNET_DIR_INPUT) || (masterConfigurationData.data_dir == PNET_DIR_IO); }
+        /**
+ 	* @brief Convenient acceleration method to tell if the subslot is an output
+ 	*/ 
+	bool  IsOutput()  const { return (masterConfigurationData.data_dir == PNET_DIR_OUTPUT) || (masterConfigurationData.data_dir == PNET_DIR_IO); }
+        /**
+ 	* @brief Convenient acceleration method to tell if the subslot is a NO-I/O (See DataSource Documentation)
+ 	*/ 
+	bool  IsNoIO()    const { return (masterConfigurationData.data_dir == PNET_DIR_NO_IO); }
+        /**
+ 	* @brief Convenient acceleration method to tell if the subslot is master advertised and present in the MARTe2 cfg file
+ 	* which means that is used in the adapter loop
+ 	*/ 
+	bool  IsUsed()    const { return (masterAdded && configAdded);  }
+        /**
+ 	* @brief Convenient acceleration method to tell if an alarm is active on the subslot
+ 	*/
+	bool  IsAlarmed() const { return alarmArmed; }
+        /**
+ 	* @brief Convenient acceleration method to clear (ack) the alarm on the subslot
+ 	*/ 
+	//lint -e{1960} Alarm acknowledgement sets the armed variable back, which status can be retrieved with the IsAlarmed function
 	void  AckAlarm()  { alarmArmed = false; }
 
     }pnet_subslot_t;
@@ -134,20 +189,41 @@ namespace ProfinetDataSourceDriver {
      *              When running, the two must match, otherwise no memory mapping can occur and no runtime plugging can happen.
     */
     typedef struct pnet_slot {
-        bool                                masterAdded;
+        /**
+ 	* @brief Indicates whether the slot was advertised by the master
+ 	*/ 
+	bool                                masterAdded;
+	/**
+ 	* @brief Indicates whether the slot was added from the MARTe2 configuration file
+ 	*/  	
         bool                                configAdded;
-
+	/**
+ 	* @brief Slot number idenifier
+ 	*/ 	
         uint16_t                            slotNumber;
-        uint32_t                            moduleIdentifier;
+        /**
+ 	* @brief Module identifier
+ 	*/ 	
+	uint32_t                            moduleIdentifier;
 
+	/**
+ 	* @brief Structure containing the subslot for the represented slot
+ 	*/
         SimpleLinkedList<pnet_subslot_t*>   subslots;
 
+	/**
+ 	* @brief Constructs and initializes an empty slot structure
+ 	*/ 
         pnet_slot() {
             masterAdded = configAdded = false;
             slotNumber = 0u;
-			moduleIdentifier = 0u;
+	    moduleIdentifier = 0u;
         }
-
+	
+	/**
+ 	* @brief Indicates if the slot was advertised by the master and added in the MARTe2 configuration file
+ 	* which essentially means that the slot is used
+ 	*/ 
         bool                                IsUsed()    const { return (masterAdded && configAdded);  }
 
     }pnet_slot_t;
@@ -156,9 +232,19 @@ namespace ProfinetDataSourceDriver {
      * @brief Describes for the specific API Identifier the AREP (application relationship identifier). Currently single AREP supported.
      */
     typedef struct pnet_api {
+	/**
+ 	* @brief Identifies the API (Currently not supported)
+ 	*/ 	
         uint32_t                            apiIdentifier;
+
+	/**
+ 	* @brief Identifies the application relatioship
+ 	*/ 
         uint32_t                            appRelationshipIdentifier;
 
+	/**
+ 	* @brief The structure which models the slot and subslot hierarchy of the module
+ 	*/ 
         SimpleLinkedList<pnet_slot_t*>      slots;
     }pnet_api_t;
 
