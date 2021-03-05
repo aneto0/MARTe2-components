76fa1e26709ef4d3acaa62671296c6c83b8af849
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2021-03-05 16:44:52 +0100

    Stub test unit plus HighResolutionTimeProvider specific

---
+++ Test/Components/DataSources/LinuxTimer/TimeProviderTest.cpp
@@ -0,0 +1,138 @@
+/**
+ * @file TimeProviderTest.cpp
+ * @brief Source file for class HighResolutionTimeProviderTest
+ * @date 05/03/2021
+ * @author Andre Neto
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class LinuxTimerTest (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+#define DLL_API
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+#include "AdvancedErrorManagement.h"
+#include "Sleep.h"
+#include "TimeProvider.h"
+#include "TimeProviderTest.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+
+
+TimeProviderTest::TimeProviderTest() {
+	timeProvider = NULL;
+}
+
+TimeProviderTest::~TimeProviderTest() {
+	if(timeProvider != NULL) {
+		delete timeProvider;	
+	}
+}
+
+
+bool TimeProviderTest::TestConstructor() {
+	return true;
+}
+
+bool TimeProviderTest::TestCounter() {
+	using namespace MARTe;
+
+	bool ok = (timeProvider != NULL);
+
+	if(ok) {
+		uint64 counterAtStart = timeProvider->Counter();
+		Sleep::Sec(1);
+		uint64 counterAtEnd = timeProvider->Counter();
+		ok = (counterAtStart < counterAtEnd);
+	}
+
+	return ok;
+}
+
+bool TimeProviderTest::TestPeriod() {
+	using namespace MARTe;
+
+	bool ok = (timeProvider != NULL);
+	
+	if(ok) {
+		ok = (timeProvider->Period() != 0);
+		if(!ok) {
+			REPORT_ERROR_STATIC(ErrorManagement::FatalError, "Period is == 0");
+		}
+	}
+
+	if(ok) {
+		ok = (timeProvider->Period() == (1.0 / timeProvider->Frequency()));
+		if(!ok) {
+			REPORT_ERROR_STATIC(ErrorManagement::FatalError, "Period value is not equivalent to the inverse of the frequency");
+		}
+	}
+	return ok;
+}
+
+bool TimeProviderTest::TestFrequency() {
+	using namespace MARTe;
+
+	bool ok = (timeProvider != NULL);
+
+	if(ok) {
+		ok = (timeProvider->Frequency() != 0);
+		if(!ok) {
+			REPORT_ERROR_STATIC(ErrorManagement::FatalError, "Frequency is == 0");
+		}
+	}
+
+	if(ok) {
+		ok = (timeProvider->Frequency() == (1.0 / timeProvider->Period()));
+		if(!ok) {
+			REPORT_ERROR_STATIC(ErrorManagement::FatalError, "Frequency value is not equivalent to the inverse of the period");
+		}
+	}
+
+	return ok;
+}
+
+bool TimeProviderTest::TestBusySleep() {
+	using namespace MARTe;
+
+	bool ok = (timeProvider != NULL);
+
+	if(ok) {
+		uint64 startTime = timeProvider->Counter();
+		uint64 deltaTime = 1000;
+		timeProvider->BusySleep(startTime, deltaTime);
+		uint64 endTime = timeProvider->Counter();
+
+		ok = ((endTime - startTime) >= deltaTime);
+		REPORT_ERROR_STATIC(ErrorManagement::Information, "Busy sleep tolerance after 1k delta %d", (endTime - startTime));
+	}
+
+	return ok;
+}
+
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
---
+++ Test/Components/DataSources/LinuxTimer/TimeProviderTest.h
@@ -0,0 +1,91 @@
+/**
+ * @file TimeProviderTest.h
+ * @brief Header file for class TimeProviderTest
+ * @date 05/03/2021
+ * @author Andre Neto
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class LinuxTimerTest
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef LINUXTIMERTEST_TIMEPROVIDERTEST_H_
+#define LINUXTIMERTEST_TIMEPROVIDERTEST_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+#include "TimeProvider.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief Tests the TimeProvider methods providing stubs for other plugin implementations
+ */
+class TimeProviderTest {
+	public:
+
+		/**
+		 * @brief Tests the constructor.
+		 */
+		TimeProviderTest();
+
+
+		/**
+		 * @brief Tests the destructor.
+		 */
+		~TimeProviderTest();
+
+		/**
+		 * @brief Tests the constructor.
+		 */
+		virtual bool TestConstructor();
+
+		/**
+		* @brief Tests the internal counter
+		*/
+		virtual bool TestCounter();
+
+		/**
+		* @brief Tests the period
+		*/
+		virtual bool TestPeriod();
+
+		/**
+		* @brief Tests the frequency
+		*/
+		virtual bool TestFrequency();
+
+		/**
+		* @brief Tests the busy sleeping feature
+		*/
+		virtual bool TestBusySleep();
+
+	protected:
+		MARTe::TimeProvider* timeProvider;
+
+};
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+#endif /* LINUXTIMERTEST_TIMEPROVIDERTEST_H_ */
+
