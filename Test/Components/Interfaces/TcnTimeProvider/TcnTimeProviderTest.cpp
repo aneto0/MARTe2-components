07a5d7e1eea6175557611e45dccb57367def6a6f
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2021-03-17 12:29:09 +0100

    TcnTimeProvider test scaffolded

---
+++ Test/Components/Interfaces/TcnTimeProvider/TcnTimeProviderTest.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file MemoryGateTest.cpp
+ * @brief Source file for class MemoryGateTest
+ * @date 24/07/2018
+ * @author Giuseppe Ferro
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class MemoryGateTest (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
+#include "AdvancedErrorManagement.h"
+#include "ConfigurationDatabase.h"
+#include "DataSourceI.h"
+#include "EventSem.h"
+#include "GAMSchedulerI.h"
+#include "IOGAM.h"
+#include "MemoryDataSourceI.h"
+#include "MemoryGateTest.h"
+#include "MemoryMapInputBroker.h"
+#include "MemoryMapOutputBroker.h"
+#include "MemoryMapOutputBroker.h"
+#include "MemoryMapSynchronisedInputBroker.h"
+#include "MemoryMapSynchronisedOutputBroker.h"
+#include "MessageI.h"
+#include "ObjectRegistryDatabase.h"
+#include "RealTimeApplication.h"
+#include "StandardParser.h"
+#include "Threads.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+
+TcnTimeProviderTest::TcnTimeProviderTest() {
+
+}
+
+TcnTimeProviderTest::~TcnTimeProviderTest() {
+
+}
+
+bool TcnTimeProviderTest::TestConstructor() {
+    return true;
+}
+
+bool TcnTimeProviderTest::TestInitialise() {
+    return true;
+}
