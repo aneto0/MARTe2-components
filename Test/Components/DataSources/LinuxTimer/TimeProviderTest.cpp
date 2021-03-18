d4b3bb8261b8c47f697a65cb8784ea44d75b50b8
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2021-03-18 11:00:12 +0100

    Corrections

--- Test/Components/DataSources/LinuxTimer/TimeProviderTest.cpp
+++ Test/Components/DataSources/LinuxTimer/TimeProviderTest.cpp
@@ -79,7 +79,8 @@ bool TimeProviderTest::TestPeriod() {
     }
 
     if(ok) {
-        ok = ((static_cast<float64>(timeProvider->Period()) * static_cast<float64>(timeProvider->Frequency())) == 1.0);
+        float64 frequencyInverse = 1.0 / static_cast<float64>(timeProvider->Frequency());
+        ok = static_cast<float64>(timeProvider->Period()) == frequencyInverse;
         if(!ok) {
             REPORT_ERROR_STATIC(ErrorManagement::FatalError, "Period value is not equivalent to the inverse of the frequency");
         }
