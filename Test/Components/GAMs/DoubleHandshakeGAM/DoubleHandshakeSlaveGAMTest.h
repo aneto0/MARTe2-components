ae40c0c63d0c4ac14bb5dbd057e7224d27a4152a
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2021-03-31 18:01:48 +0200

    First DoubleHandshakeGAM merge

---
+++ Test/Components/GAMs/DoubleHandshakeGAM/DoubleHandshakeSlaveGAMTest.h
@@ -0,0 +1,87 @@
+/**
+ * @file DoubleHandshakeSlaveGAMTest.h
+ * @brief Header file for class DoubleHandshakeSlaveGAMTest
+ * @date 13 ago 2019
+ * @author pc
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class DoubleHandshakeSlaveGAMTest
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef TEST_COMPONENTS_GAMS_DOUBLEHANDSHAKEGAM_DOUBLEHANDSHAKESLAVEGAMTEST_H_
+#define TEST_COMPONENTS_GAMS_DOUBLEHANDSHAKEGAM_DOUBLEHANDSHAKESLAVEGAMTEST_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+#include "DoubleHandshakeSlaveGAM.h"
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+
+using namespace MARTe;
+
+class DoubleHandshakeSlaveGAMTest {
+public:
+    DoubleHandshakeSlaveGAMTest();
+    virtual ~DoubleHandshakeSlaveGAMTest();
+
+    bool TestConstructor();
+
+    bool TestSetup();
+
+    bool TestSetup_UselessSignal();
+
+    bool TestSetup_NumberOfElements();
+
+    bool TestSetup_FalseNCommansDiffThanNAcks();
+
+    bool TestSetup_FalseNCommansDiffThanNClears();
+
+    bool TestSetup_FalseNCommansDiffThanNStatuses();
+
+    bool TestSetup_FalseClearTrigsWrongType();
+
+    bool TestSetup_FalseInternalStateWrongType();
+
+    bool TestSetup_FalseCommandAndAckMismatch();
+
+    bool TestExecute();
+
+    bool TestExecute_ErrorFromSending();
+
+    bool TestExecute_ErrorFromDone();
+
+    bool TestExecute_ErrorFromSendingTimeout();
+
+    bool TestExecute_ErrorFromDoneTimeout();
+
+    bool TestExecute_RecoverFromError();
+
+    bool TestExecute_Interactive();
+
+};
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+#endif /* TEST_COMPONENTS_GAMS_DOUBLEHANDSHAKEGAM_DOUBLEHANDSHAKESLAVEGAMTEST_H_ */
+
