184b74a98b46c889a7453708e4c1cf091d9b074e
André Neto <Andre.Neto@f4e.europa.eu>
2025-09-22 15:01:57 +0200

    Small header corrections

--- Source/Components/Interfaces/ApplicationKiller/ApplicationKiller.h
+++ Source/Components/Interfaces/ApplicationKiller/ApplicationKiller.h
@@ -1,26 +1,28 @@
 /**
  * @file ApplicationKiller.h
  * @brief Header file for class ApplicationKiller
- * @date 30/08/2019
+ * @date 24/04/2025
+ * @author Llorenc Capella
  *
- * @copyright Copyright 2019 EPFL SPC Lausanne
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
  * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence")
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
  *
- * @warning Unless required by applicable law or agreed to in writing,
+ * @warning Unless required by applicable law or agreed to in writing, 
  * software distributed under the Licence is distributed on an "AS IS"
  * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the Licence permissions and limitations under the Licence.
 
- * @details This header file contains the declaration of the class ApplicationKillerGAM
+ * @details This header file contains the declaration of the class ApplicationKiller 
  * with all of its public, protected and private members. It may also include
  * definitions for inline methods which need to be visible to the compiler.
  */
 
-#ifndef SPCSHOTMANAGER_H_
-#define SPCSHOTMANAGER_H_
+#ifndef APPLICATION_KILLER_H_
+#define APPLICATION_KILLER_H_
 
 /*---------------------------------------------------------------------------*/
 /*                        Standard header includes                           */
@@ -168,5 +170,5 @@ private:
 /*                        Inline method definitions                          */
 /*---------------------------------------------------------------------------*/
 
-#endif /* SPCSHOTMANAGER_H_ */
+#endif /* APPLICATION_KILLER_H_ */
 
